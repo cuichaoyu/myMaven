3cf204db8a7552fe1bb4440ff9f6411bfa61ffd8
ccy <941112757@qq.com>
2018-08-17 12:49:08 +0800

    sll

--- src/main/java/test/ccyTest.java
+++ src/main/java/test/ccyTest.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @Controller
 @EnableAutoConfiguration
 public class ccyTest {
 
+	/**
+	 * 
+	    * @Title: home
+	    * @Description: TODO(首页)
+	    * @param @return    参数
+	    * @return String    返回类型
+	    * @throws
+	 */
 	@RequestMapping(value="/")//是springmvc中的注解
     String home(){
         return "helloworld";
